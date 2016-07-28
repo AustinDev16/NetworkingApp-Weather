13e04cc45de404bde6694c377853bdb6c6a6400d
AustinDev16 <austin.blaser@gmail.com>
2016-07-28 15:01:11 -0600

    Initial Commit

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Austin on 7/28/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipProbability: Double
+    let summary: String
+    let icon: UIImage
+}
